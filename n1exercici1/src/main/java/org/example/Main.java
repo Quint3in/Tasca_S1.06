253e6f7f9bf8f671f19dfbd1e1bea603e4d31fea
JoelQuintana <joelqn95@gmail.com>
2026-03-12 11:04:18 +0100

    feat(n1exercici1): add NoGenericMethods and demo argument order

---
+++ n1exercici1/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        String one = "one";
+        Integer two = 2;
+        Double three = 3.0;
+        NoGenericMethods a = new NoGenericMethods(one, two, three);
+        NoGenericMethods b = new NoGenericMethods(two, three, one);
+
+        System.out.println(a.getElement1());
+        System.out.println(b.getElement1());
+    }
+}
